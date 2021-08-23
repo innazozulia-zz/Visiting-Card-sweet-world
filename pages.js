22ce36d923762da5176f87562107578a6df31d8e
innazozulia <innazozulia@outlook.com>
2021-08-23 10:51:31 +0300

    adding main files

---
+++ pages.js
@@ -0,0 +1,42 @@
+const pages = [
+  {
+    title: "about",
+    content:
+      '<div class="about"> <img src="./img/ann.jpg"> <p>Hi! I am Ann Shitaki. I am a pastry chef with over 10 years of experience.  I wrote the recipe book. It’ll make you feel like a real pastry chef.</p> </div>',
+      
+  },
+  {
+    title: "sweets",
+    content: `<div class ="sweets">
+        <figure> 
+            <img src="./img/drinks.jpg" alt="drinks"> 
+            <figcaption>Coktails and Drinks</figcaption> 
+        </figure> 
+        <figure> 
+            <img src="./img/donuts.jpg" alt="donuts">
+             <figcaption>Classic Donuts</figcaption>
+        </figure> 
+        <figure> <img src="./img/icecream.jpg" alt="icecreams"> <figcaption>Icecreams</figcaption> </figure> 
+        <figure> <img src="./img/candys.jpg" alt="candys"> <figcaption>Sweets</figcaption> </figure>         
+        </div>
+     `
+  },
+  {
+    title: "prices",
+    content: `<div class="prices">
+            <p>Coktails and Drinks - 30$</p>
+            <p>Classic Donuts - 45$</p>
+            <p>Icecreams - 50$</p>
+            <p>Sweets - 60$</p>
+        </div>`
+  },
+  {
+    title: "contacts",
+    content: `<div class='contacts'>
+            <p>Аdres: La pharma de Répu
+            5 place de la République, 75003 Paris</p>
+            <p>+33 (0) 1 47 00 18 08</p>
+            <p>e-mail:annsweets@gmail.com</p>
+        </div>`
+  }
+];
